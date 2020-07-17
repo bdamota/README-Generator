805160cabd73aa00c5868977f4dc0fd8739295f9
LYNN DAMOTA <bdamota0506@gmail.com>
2020-07-16 17:56:11 -0700

    queastions added to index.js

--- src/page-template.js
+++ src/page-template.js
@@ -91,7 +91,7 @@ module.exports = templateData => {
    ${generateProjects(projects)}
     </main>
     <footer class="container text-center py-3">
-      <h3 class="text-dark">&copy; ${new Date().getFullYear()}</h3>
+      <h5 class="text-dark">&copy; ${new Date().getFullYear()}</h5>
     </footer>
   </body>
   </html>
